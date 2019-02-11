eb181928b0fa4e4e7ff65143fe6d3450c8d56495
ImNotAGay <43098725+iLoveGachimuchi@users.noreply.github.com>
2019-02-11 22:57:57 +0300

    Add files via upload

---
+++ cs_Test/cs_Test/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace cs_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Try to click me";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.tmrDef_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(140, 140);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseEnter += new System.EventHandler(this.form_MouseEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ cs_Test/cs_Test/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Timers;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+
+namespace cs_Test
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+        static protected int currX;
+        static protected int currY;
+        static protected int diffX;
+        static protected int diffY;
+
+
+        private rnd rand = new rnd();
+
+
+        public int newXPoint;
+        public int newYPoint;
+
+        public void newLocation()
+        {
+
+            this.newXPoint = rand.GetRandomNumber(1, 1900 - 140);
+            this.newYPoint = rand.GetRandomNumber(1, 1080 - 140);
+            bool circlFlag = (Cursor.Position.X + 140 > newXPoint || Cursor.Position.X < newXPoint) && (Cursor.Position.Y + 141 > newYPoint || Cursor.Position.Y < newYPoint);
+            while (!circlFlag)
+            {
+                newXPoint = rand.GetRandomNumber(1, 1900 - 140);
+                newYPoint = rand.GetRandomNumber(1, 1070 - 140);
+                circlFlag = (Cursor.Position.X + 141 > newXPoint || Cursor.Position.X < newXPoint) && (Cursor.Position.Y + 141 > newYPoint || Cursor.Position.Y < newYPoint);
+            }
+            this.Location = new Point(newXPoint, newYPoint);
+            /* if (timerNewLocation != null)
+                 timerNewLocation.Dispose();
+             timerNewLocation = new System.Timers.Timer(0.000003);
+             timerNewLocation.Interval = 0.000003;
+             timerNewLocation.Elapsed += new ElapsedEventHandler(timer_newElapced);
+             timerNewLocation.Start();*/
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+            this.StartPosition = FormStartPosition.Manual;
+
+
+            newLocation();
+            timer1.Start();
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.P)
+            {
+                newLocation();
+            }
+            if (e.KeyCode == Keys.Q)
+            {
+                this.Dispose();
+                Application.Exit();
+            }
+        }
+
+        private void form_MouseEnter(object sender, EventArgs e)
+        {
+            newLocation();
+        }
+
+
+        private void goToPosit(ref int pos, int min, int max)
+        {
+            int tmp = 0;
+            for (int i = 0; i < 9; i++)
+                tmp += rand.GetRandomNumber(min, max);
+            pos = tmp / 10;
+        }
+
+
+        private void tmrDef_Tick(object sender, EventArgs e)
+        {
+            Point defPnt = new Point();
+            GetCursorPos(ref defPnt);
+            int xDif = 0;
+            int yDif = 0;
+            if (currX != defPnt.X || currY != defPnt.Y)
+            {
+                //Блок находится слева
+                if (1920 / 2 < this.Location.X)
+                {
+                    //Курсор близко слева
+                    if (1920 / 2 - currX < 0)
+                        goToPosit(ref xDif, -50, 100);
+                    else
+                        goToPosit(ref xDif, -100, 50);
+                }
+                //Блок находится справа
+                else
+                {
+                    //Курсор близко слева
+                    if (1920 / 2 - currX < 0)
+                        goToPosit(ref xDif, -100, 50);
+                    else
+                        goToPosit(ref xDif, -50, 100); 
+                }
+                //Блок находится сверху
+                if (1080 / 2 < this.Location.X)
+                {
+                    //Курсор близко снизу
+                    if (1080 / 2 - currY < 0)
+                        goToPosit(ref yDif, -43, 76);
+                    else
+                        goToPosit(ref yDif, -76, 43);
+                }
+                //Блок находится снизу
+                else
+                {
+                    if (1080 / 2 - currY < 0)
+                        goToPosit(ref yDif, -76, 43); 
+                    else
+                        goToPosit(ref yDif, -43, 76);
+
+                }
+                currX = defPnt.X + xDif;
+                currY = defPnt.Y + yDif;
+
+
+            }
+
+            Cursor.Position = new Point(currX, currY);
+            //this.Location = new Point(rand.GetRandomNumber(3, 1020), rand.GetRandomNumber(3, 1900));
+        }
+
+    }
+}
